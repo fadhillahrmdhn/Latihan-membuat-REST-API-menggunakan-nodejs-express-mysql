6cd686b8deb9253a22162db43d37f58f4709af8d
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-11-17 08:12:43 +0700

    membuat controller

---
+++ controllers/studentController.js
@@ -0,0 +1,51 @@
+//@desc mengambil seluruh data mahasiswa
+//@route GET /api/students
+//access public
+exports.getAllStudents=(req,res,next)=>{
+    res.status(200).json({
+        succes: 'true',
+        message: 'melihat seluruh data mahasiswa',
+      });
+}
+
+//@desc menambahkan data mahasiswa
+//@route POST /api/students
+//access public
+exports.createStudent=(req,res,next)=>{
+    res.status(200).json({
+        succes: 'true',
+        message: 'menambahkan data mahasiswa',
+      });
+}
+
+//@desc melihat data satu mahasiswa
+//@route GET /api/students/:id
+//access public
+exports.getStudent=(req,res,next)=>{
+    res.status(200).json({
+        succes: 'true',
+        message: `melihat data mahasiswa ke-${req.params.id}`,
+      });
+}
+
+//@desc memperbaharui data mahasiswa
+//@route PUT /api/students/:id
+//access public
+exports.updateStudent=(req,res,next)=>{
+    res.status(200).json({
+        succes: 'true',
+        message: `memperbaharui data mahasiswa ke-${req.params.id}`,
+      });
+}
+
+//@desc memhapus data mahasiswa
+//@route DELETE /api/students/:id
+//access public
+exports.deleteStudent=(req,res,next)=>{
+    res.status(200).json({
+        succes: 'true',
+        message: `menghapus data mahasiswa ke-${req.params.id}`,
+      });
+}
+
+
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,40 +1,12 @@
 const express = require('express');
+const {
+  getAllStudents,createStudent,getStudent,deleteStudent,updateStudent
+}=require('../controllers/studentController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({
-    succes: 'true',
-    message: 'melihat seluruh data mahasiswa',
-  });
-});
+router.route('/').get(getAllStudents).post(createStudent);
 
-router.post('/', (req, res) => {
-  res.status(200).json({
-    succes: 'true',
-    message: 'menambahkan data mahasiswa',
-  });
-});
-
-router.get('/:id', (req, res) => {
-  res.status(200).json({
-    succes: 'true',
-    message: `melihat data mahasiswa ke-${req.params.id}`,
-  });
-});
-
-router.put('/:id', (req, res) => {
-  res.status(200).json({
-    succes: 'true',
-    message: `memperbaharui data mahasiswa ke-${req.params.id}`,
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({
-    succes: 'true',
-    message: `menghapus data mahasiswa ke-${req.params.id}`,
-  });
-});
+router.route('/:id').get(getStudent).put(updateStudent).delete(deleteStudent);
 
 module.exports = router;
