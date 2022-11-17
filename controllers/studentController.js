8aec777227faaee0e85a5e12ae7f651b6790d276
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-11-17 21:47:10 +0700

    Memperbarui Data Mahasiswa

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,37 +2,36 @@ const Student = require('../models/studentModel');
 //@desc mengambil seluruh data mahasiswa
 //@route GET /api/students
 //access public
-exports.getAllStudents= async(req,res,next)=>{
-    try{
-        const student = await Student.findAll();
+exports.getAllStudents = async (req, res, next) => {
+  try {
+    const student = await Student.findAll();
 
-        res.status(200).json({
-        succes: true,
-        message: 'melihat seluruh data mahasiswa',
-        data: student,
-      });
-    }catch{
-      res.status(500).json({
-        succes: false,
-        message:'something went wrong'
-      });
-    }
+    res.status(200).json({
+      succes: true,
+      message: 'melihat seluruh data mahasiswa',
+      data: student,
+    });
+  } catch {
+    res.status(500).json({
+      succes: false,
+      message: 'something went wrong',
+    });
+  }
 };
 
 //@desc menambahkan data mahasiswa
 //@route POST /api/students
 //access public
-exports.createStudent= async(req,res,next)=>{
-
-  try{
+exports.createStudent = async (req, res, next) => {
+  try {
     const student = await Student.create(req.body);
 
     res.status(200).json({
-        succes: true,
-        message: 'berhasil menambahkan data mahasiswa',
-        data: student,
-      });
-  }catch(err){
+      succes: true,
+      message: 'berhasil menambahkan data mahasiswa',
+      data: student,
+    });
+  } catch (err) {
     res.status(400).json({
       succes: false,
       message: err.message,
@@ -43,48 +42,67 @@ exports.createStudent= async(req,res,next)=>{
 //@desc melihat data satu mahasiswa
 //@route GET /api/students/:id
 //access public
-exports.getStudent= async(req,res,next)=>{
-    try{
-      const student = await Student.findByPk(req.params.id);
+exports.getStudent = async (req, res, next) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
 
-      if(!student){
-        return res.status(404).json({
-          succes: false,
-          message:'data mahasiswa tidak ditemukan'
-        });
-      }
-
-      res.status(200).json({
-      succes: true,
-      message: `melihat data mahasiswa ke-${req.params.id}`,
-      data:student,
-      });
-    }catch(err){
-      res.status(400).json({
-      succes: false,
-      message: err.message,
+    if (!student) {
+      return res.status(404).json({
+        succes: false,
+        message: 'data mahasiswa tidak ditemukan',
       });
     }
+
+    res.status(200).json({
+      succes: true,
+      message: `melihat data mahasiswa ke-${req.params.id}`,
+      data: student,
+    });
+  } catch (err) {
+    res.status(400).json({
+      succes: false,
+      message: err.message,
+    });
+  }
 };
 
 //@desc memperbaharui data mahasiswa
 //@route PUT /api/students/:id
 //access public
-exports.updateStudent=(req,res,next)=>{
-    res.status(200).json({
-        succes: true,
-        message: `memperbaharui data mahasiswa ke-${req.params.id}`,
+exports.updateStudent = async (req, res, next) => {
+  try {
+    const student = await Student.findByPk(req.params.id);
+
+    if (!student) {
+      return res.status(404).json({
+        succes: false,
+        message: 'data mahasiswa tidak ditemukan',
       });
+    }
+
+    await student.update(req.body);
+
+    res.status(200).json({
+      succes: true,
+      message: `data berhasil diperbaharui data mahasiswa`,
+      data:student
+    });
+  } catch (err) {
+    res.status(400).json({
+      succes: false,
+      message: err.message,
+    });
+  }
 };
 
 //@desc memhapus data mahasiswa
 //@route DELETE /api/students/:id
 //access public
-exports.deleteStudent=(req,res,next)=>{
-    res.status(200).json({
-        succes: true,
-        message: `menghapus data mahasiswa ke-${req.params.id}`,
-      });
+exports.deleteStudent = (req, res, next) => {
+  try {
+  } catch (err) {}
+  res.status(200).json({
+    succes: true,
+    message: `menghapus data mahasiswa ke-${req.params.id}`,
+  });
 };
-
-
