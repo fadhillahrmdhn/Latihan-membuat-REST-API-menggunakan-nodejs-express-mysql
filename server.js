d88c556d5405e23276aecacfe4420403d488818a
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-11-17 10:12:25 +0700

    setup dan kofigurasi database

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 require('dotenv').config();
+const sequelize = require('./utils/database');
 
 const studentRoutes = require('./routes/studentRoutes');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    req.hello='hello world';
+app.use((req, res, next) => {
+  req.hello = 'hello world';
 
-    next();
+  next();
 });
 
-app.use('/api/students',studentRoutes);
+app.use('/api/students', studentRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode port ${PORT}`));
+sequelize.authenticate().then(()=>{
+    console.log('database berhasil disambungkan');
+    app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode port ${PORT}`));
+}).catch(()=>{
+    console.log('database gagal disambungkan');
+});
